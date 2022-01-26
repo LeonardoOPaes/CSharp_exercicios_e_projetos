7a849cfd1085723bf8cd4052dd33457db5d98c7e
Leonardo <leonardopaes@gmail.com>
2022-01-26 19:53:00 -0300

    finalização projeto

--- projetoConsumoAPI/Program.cs
+++ projetoConsumoAPI/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace projetoConsumoAPI
+﻿using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+
+
+namespace projetoConsumoAPI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Cadastra a requisição
+            var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos");
+            requisicao.Method = "GET";
+
+            //Executa a requisição
+            using (var resposta = requisicao.GetResponse())
+            {
+                var stream = resposta.GetResponseStream(); // Pega a Stream
+                StreamReader leitor = new StreamReader(stream); // Decodifica a Stream
+                object dados = leitor.ReadToEnd(); // Passa o resultado aqui
+
+                List<tarefa> tarefas = JsonConvert.DeserializeObject<List<tarefa>>(dados.ToString());
+
+                foreach (var item in tarefas)
+                {
+                    item.Exibir();
+                }
+
+                Console.WriteLine(tarefas);
+
+                stream.Close();
+                resposta.Close();
+            }
+
         }
     }
 }
